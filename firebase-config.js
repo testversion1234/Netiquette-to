da2f63fbec03663e5c57cf9d033f8e83fe20318a
testversion1234 <hetebrueg@me.com>
2025-10-03 09:57:20 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // firebase-config.js — trage hier deine Web-App Config ein
 export const FALLBACK_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_INSTANCE_ID-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+ apiKey: "AIzaSyBK9iwsc9kpAW006Jmh197ToCSQbTk-X34",
+  authDomain: "netiquette-74729.firebaseapp.com",
+  databaseURL: "https://netiquette-74729-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "netiquette-74729",
+  storageBucket: "netiquette-74729.firebasestorage.app",
+  messagingSenderId: "178454261761",
+  appId: "1:178454261761:web:89dbb4e052766b550a5000",
+  measurementId: "G-3LYGL489D9"
 };
